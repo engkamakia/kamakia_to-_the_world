8b283da4c93af8960ffadc8f84d17d088d4cea9d
engkamakia <georgekamakia@gmail.com>
2023-11-15 13:44:27 +0300

    create the reference function onUpdateActiveLink which activates the links on Navigation bar

--- personal_portfolio/src/components/NavBar.js
+++ personal_portfolio/src/components/NavBar.js
@@ -7,6 +7,24 @@ export const NavBar = () => {
     const [activeLink, setActiveLink] = useState("home");
     const [scrolled, setScrolled] = useState(false);
 
+    useEffect(() => {
+      const onScroll = () => {
+        if (window.scrollY > 50) {
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
+    const onUpdateActiveLink = (value) => {
+      setActiveLink(value);
+    };
+
   return (    
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
